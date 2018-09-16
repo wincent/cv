add351b8913c1ef094b42e5049c2188da253a346
Greg Hurrell <greg@hurrell.net>
2018-09-16 17:44:43 +0200

    Approximate the design of the Pages template
    Adding enough content here, and switching to a better font, to more
    closely approximate what you get in the Pages template. These fonts
    still look a bit too heavy to me, so I am going to scope out some
    alternatives.

--- index.js
+++ index.js
@@ -13,13 +13,73 @@ doc
     'baskerville',
     'fonts/Libre_Baskerville/LibreBaskerville-Regular.ttf',
   )
-  .registerFont('didot', 'fonts/Abril_Fatface/AbrilFatface-Regular.ttf')
+  .registerFont('didot', 'fonts/Playfair_Display/PlayfairDisplay-Bold.ttf')
+  .registerFont(
+    'didot-regular',
+    'fonts/Playfair_Display/PlayfairDisplay-Regular.ttf',
+  );
+
+const date = dateString => {
+  // We only want to show the year.
+  return dateString.toString().replace(/^(\d{4}).*/, '$1');
+};
+
+const capitalize = string => {
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
+};
+
+const heading = text => doc
   .font('didot')
+  .fontSize(11)
+  .moveDown()
+  .text(text.toUpperCase(), {characterSpacing: 2});
+
+const subHeading = text => doc
+  .font('didot')
+  .fontSize(11)
+  .moveDown()
+  .text(text, {characterSpacing: .5});
+
+const para = text => doc
+  .font('baskerville')
+  .fontSize(12)
+  .text(text);
+
+doc
+  .font('didot-regular')
+  .fontSize(16)
   .text(data.identity.name, {align: 'right'})
   .font('baskerville')
   .fontSize(8)
   .moveDown()
-  .text('123 Main Street', {align: 'right'});
+  .text('123 Main Street', {align: 'right'})
+  .text('12345 Madrid, Spain', {align: 'right'})
+  .text('555-555-5555', {align: 'right'})
+  .text(data.identity.email, {align: 'right'});
+
+heading('Profile');
+para(data.profile);
+
+const ENDASH = '\u2013';
+const EMDASH = '\u2014';
+
+heading('Experience');
+data.experience.forEach(({role, company, location, from, to, description}) => {
+  subHeading(
+    `${role}, ${company}; ${location} ${EMDASH} ${date(from)}${ENDASH}${date(to)}`
+  );
+  para(description);
+});
+
+heading('Education');
+data.education.forEach(({institution, graduated, qualification}) => {
+  para(`${institution}, ${date(graduated)} ${EMDASH} ${qualification}`);
+});
+
+heading('Skills');
+Object.keys(data.skills).forEach(skill => {
+  para(capitalize(skill) + ': ' + data.skills[skill].join(', ') + '.');
+});
 
 doc.pipe(fs.createWriteStream('cv.pdf'));
 doc.end();
