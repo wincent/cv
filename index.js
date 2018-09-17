cdc791e6687a85820d3ab999103965d47a11c5ef
Greg Hurrell <greg@hurrell.net>
2018-09-17 21:00:25 +0200

    Prettier

--- index.js
+++ index.js
@@ -382,13 +382,12 @@ function seed(i) {
   m_z = 987654321;
 }
 
-function random()
-{
-    m_z = (36969 * (m_z & 65535) + (m_z >> 16)) & mask;
-    m_w = (18000 * (m_w & 65535) + (m_w >> 16)) & mask;
-    let result = ((m_z << 16) + m_w) & mask;
-    result /= 4294967296;
-    return result + 0.5;
+function random() {
+  m_z = (36969 * (m_z & 65535) + (m_z >> 16)) & mask;
+  m_w = (18000 * (m_w & 65535) + (m_w >> 16)) & mask;
+  let result = ((m_z << 16) + m_w) & mask;
+  result /= 4294967296;
+  return result + 0.5;
 }
 
 Math.random = random;
